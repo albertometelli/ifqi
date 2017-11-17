2f0838617e02b2d8d1c4d062be40e4967adc2d27
muttimirco <33064862+muttimirco@users.noreply.github.com>
2017-11-17 10:59:34 +0100

    Execution time added

--- examples/simple_run_race_track.py
+++ examples/simple_run_race_track.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from ifqi.envs.race_track import RaceTrackEnv
 from ifqi.evaluation import evaluation
 
@@ -7,7 +8,10 @@ class RandomPolicy(object):
     def draw_action(self, state, done):
         return np.random.choice(5)
 
+startTime = time.time()
 
 mdp = RaceTrackEnv(track_file='track1')
 policy = RandomPolicy()
 trajectories = evaluation.collect_episodes(mdp, policy,  n_episodes=10)
+
+print('The script took {0} seconds'.format(time.time() - startTime))
--- examples/simple_run_race_track_configurable.py
+++ examples/simple_run_race_track_configurable.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from ifqi.envs.race_track_configurable import RaceTrackConfigurableEnv
 from ifqi.evaluation import evaluation
 
@@ -7,7 +8,10 @@ class RandomPolicy(object):
     def draw_action(self, state, done):
         return np.random.choice(5)
 
+startTime = time.time()
 
 mdp = RaceTrackConfigurableEnv(track_file='track1', initial_configuration=0.5)
 policy = RandomPolicy()
 trajectories = evaluation.collect_episodes(mdp, policy,  n_episodes=10)
+
+print('The script took {0} seconds'.format(time.time() - startTime))
