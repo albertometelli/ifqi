d41caf79fe7aa4cf6dd4bad07f3052e117ebc747
T3p <matteo.papini@polimi.it>
2018-01-04 22:27:44 +0100

    added plotting example

--- script/plot_history.py
+++ script/plot_history.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-history = np.load('../results/history_04-01-2018_18-32-02.npy')
+history = np.load('../results/history_example.npy')
 index = 2 #1: theta, 2: avg return, 3: gradient
 plt.plot(range(history.shape[0]),history[:,index])
 plt.show()
