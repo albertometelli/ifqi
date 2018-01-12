fixed M2 calculation

ifqi/algorithms/policy_gradient/policy.py
@@ -137,7 +137,7 @@ class GaussianPolicyLinearMean(ParametricPolicy):
             return gradient.ravel()
         else:
             return gradient
-
+    '''
     def M_2(self, other):
         covar_diff = 2 * other.covar - self.covar
         inv_covar_diff = la.inv(covar_diff)
@@ -166,6 +166,21 @@ class GaussianPolicyLinearMean(ParametricPolicy):
             return gradient.ravel()
         else:
             return gradient
+    '''
+    def gradient_M_2(self, other):
+        gK = other.covar / (np.sqrt(self.covar * (2*other.covar - self.covar))) * 2 \
+            * self.max_state ** 2 / (2*other.covar - self.covar) * (self.K - other.K) * \
+            np.exp(la.norm(self.K - other.K) ** 2 * self.max_state ** 2 / (2*other.covar - self.covar))
+        gL = (-other.covar / (self.covar * np.sqrt(2*other.covar - self.covar)) + \
+             other.covar / (2*other.covar - self.covar) ** (3./2) +\
+             2 * self.max_state ** 2 * other.covar * la.norm(self.K - other.K) ** 2 / (2*other.covar - self.covar) ** (5./2)) * \
+             np.exp(la.norm(self.K - other.K) ** 2 * self.max_state ** 2 / (2 * other.covar - self.covar))
+        #print("gradient M_2 %s" % np.array([np.asscalar(gK), np.asscalar(gL)]))
+        return np.array([np.asscalar(gK), np.asscalar(gL)])
+
+    def M_2(self, other):
+        return np.asscalar(other.covar / (np.sqrt(self.covar * (2*other.covar - self.covar))) *\
+               np.exp(la.norm(self.K - other.K) ** 2 * self.max_state ** 2 / (2*other.covar - self.covar)))
 
 class GaussianPolicyLinearMeanCholeskyVar(ParametricPolicy):
     '''
@@ -279,6 +294,21 @@ class GaussianPolicyLinearMeanCholeskyVar(ParametricPolicy):
         else:
             return gradient_K, gradient_Lambda
 
+    def gradient_M_2(self, other):
+        gK = other.covar / (np.sqrt(self.covar * (2*other.covar - self.covar))) * 2 \
+            * self.max_state ** 2 / (2*other.covar - self.covar) * (self.K - other.K) * \
+            np.exp(la.norm(self.K - other.K) ** 2 * self.max_state ** 2 / (2*other.covar - self.covar))
+        gL = (-other.covar / (self.covar * np.sqrt(2*other.covar - self.covar)) + \
+             other.covar / (2*other.covar - self.covar) ** (3./2) +\
+             2 * self.max_state ** 2 * other.covar * la.norm(self.K - other.K) ** 2 / (2*other.covar - self.covar) ** (5./2)) * \
+             np.exp(la.norm(self.K - other.K) ** 2 * self.max_state ** 2 / (2 * other.covar - self.covar))
+        print("gradient M_2 %s" % np.array([np.asscalar(gK), np.asscalar(gL)]))
+        return np.array([np.asscalar(gK), np.asscalar(gL)])
+
+    def M_2(self, other):
+        return np.asscalar(other.covar / (np.sqrt(self.covar * (2*other.covar - self.covar))) *\
+               np.exp(la.norm(self.K - other.K) ** 2 * self.max_state ** 2 / (2*other.covar - self.covar)))
+    '''
     def M_2(self, other):
         covar_diff = 2 * other.covar - self.covar
         inv_covar_diff = la.inv(covar_diff)
@@ -289,6 +319,7 @@ class GaussianPolicyLinearMeanCholeskyVar(ParametricPolicy):
         return la.det(other.covar) ** self.dimension / la.det(self.covar) ** (self.dimension / 2.) / \
                 la.det(covar_diff) ** (self.dimension / 2.) * \
                np.exp(.5 * max_eigval * self.max_state ** 2)
+
 
     def gradient_M_2(self, other, vectorize=True):
         covar_diff = 2 * other.covar - self.covar
@@ -305,12 +336,11 @@ class GaussianPolicyLinearMeanCholeskyVar(ParametricPolicy):
         gradient_Lambda = -other.covar / (self.covar * np.sqrt(2*other.covar - self.covar)) + other.covar / (2*other.covar - self.covar)**(3./2) + 2 * (.5 * max_eigval * self.max_state ** 2) * other.covar / (2*other.covar - self.covar)**(5./2)
         gradient_Lambda *= np.exp(.5 * max_eigval * self.max_state ** 2)
 
-        '''
-        ONLY 1 DIMENSION!!!
-        '''
+        #ONLY 1 DIMENSION!!!
+
 
         return np.array([np.asscalar(gradient_K), np.asscalar(gradient_Lambda)])
-
+    '''
 
 class RBFGaussianPolicy(ParametricPolicy):
 

ifqi/algorithms/policy_gradient/policy_gradient_learner.py
@@ -213,7 +213,7 @@ class PolicyGradientLearner(object):
             print('Bound: %s' % self.bound.__class__)
             print('Trajectory generator: %s' % self.trajectory_generator.__class__)
 
-        gradient, avg_return, penalization, H_star = self.estimator.estimate(baseline_type=self.baseline_type)
+        gradient, avg_return, penalization, H_star, terminate = self.estimator.estimate(baseline_type=self.baseline_type)
 
         if return_history:
             history = [[np.copy(theta), avg_return, gradient, penalization, H_star]]
@@ -223,7 +223,7 @@ class PolicyGradientLearner(object):
         if self.verbose >= 1:
             print('Ite %s: return %s - gradient norm %s' % (ite, avg_return, gradient_norm))
 
-        while ite < self.max_iter_opt and gradient_norm > self.tol_opt:
+        while ite < self.max_iter_opt and gradient_norm > self.tol_opt: #and not terminate:
 
             theta = self.gradient_updater.update(gradient) #Gradient ascent update
             if self.verbose >= 1:
@@ -231,9 +231,13 @@ class PolicyGradientLearner(object):
 
             self.target_policy.set_parameter(theta)
             self.estimator.set_target_policy(self.target_policy)
-            gradient, avg_return, penalization, H_star  = self.estimator.estimate(baseline_type=self.baseline_type)
+            gradient, avg_return, penalization, H_star, terminate  = self.estimator.estimate(baseline_type=self.baseline_type)
+
             if return_history:
                 history.append([np.copy(theta), avg_return, gradient, penalization, H_star])
+
+            #if terminate:
+            #   break
 
             gradient_norm = la.norm(gradient)
             ite += 1
@@ -241,6 +245,7 @@ class PolicyGradientLearner(object):
             if self.verbose >= 1:
                 print('Ite %s: return %s - gradient norm %s - penalization %s' % (
                 ite, avg_return, gradient_norm, penalization))
+                print(terminate)
 
         if return_history:
             return theta, history
@@ -523,12 +528,24 @@ class GradientEstimator(object):
                                 ws[:, :, np.newaxis] * \
                                 (traj_returns[:, :, np.newaxis] - baseline), axis=1), axis=0)
         print("gradient estimate %s" % gradient_estimate)
+
+
+        if la.norm(penalization_gradient) >= la.norm(gradient_estimate):
+            terminate = True
+        else:
+            terminate = False
+
+
+
         gradient_estimate = gradient_estimate + self.gamma ** k * penalization_gradient
 
 
         if self.verbose:
             print("penalization gradient %s" % penalization_gradient)
-        return gradient_estimate, np.mean(np.sum(traj_returns, axis=1)), penalization, H_star
+
+
+
+        return gradient_estimate, np.mean(np.sum(traj_returns, axis=1)), penalization, H_star, terminate
 
 class ReinforceGradientEstimator(GradientEstimator):
 

script/run_lqg_bound_profile.py
@@ -12,16 +12,16 @@ from mpl_toolkits.mplot3d import Axes3D
 mdp = LQG1D()
 K_opt = mdp.computeOptimalK()
 
-sb = 2.
-st = 1.99
-mub = -0.2
-mut = 0.
-N = 36
+sb = 1.
+st = .99
+mub = -0.5
+mut = -0.5
+N = 1000
 
-K_grid = np.arange(-0.7, 0.1, 0.1)
-#K_grid = np.array([-0.2])
-c_grid = np.arange(0., 1.1, 0.1)
-#c_grid = np.array([1.])
+#K_grid = np.arange(-0.7, 0.1, 0.1)
+K_grid = np.arange(-0.7, -0.4, 0.03)
+#c_grid = np.arange(0.9, 1.1, 0.1)
+c_grid = np.array([1.])
 
 K, C = np.meshgrid(K_grid, c_grid)
 
@@ -85,7 +85,7 @@ def variance_Bernstein(dataset, behavioral_policy, target_policy, gamma, h, k, c
         2 * (1 + c ** 2 * (M_2 ** h - 1)) * np.log(1 / delta) / N))
 
 
-h = 10
+h = 15
 delta = 0.2
 
 bound_Hoeffding = np.zeros(K.shape)
@@ -124,7 +124,7 @@ for i in range(K.shape[0]):
         print(
         'Parameter = %s \t Shrinkage = %s \t Bias = %s\t J_hat = %s \t bound = %s' % (
         k, c, bias_, J,  bound_Chebychev[i, j]))
-'''
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 surf = ax.plot_surface(K, C , bound_Hoeffding, cmap=cm.coolwarm,
@@ -135,7 +135,6 @@ ax.set_xlabel('parameter')
 ax.set_ylabel('shrinkage')
 ax.set_zlabel('bound')
 ax.set_title('Hoeffding')
-'''
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -148,7 +147,7 @@ ax.set_ylabel('shrinkage')
 ax.set_zlabel('bound')
 ax.set_title('Chebychev')
 
-'''
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 surf = ax.plot_surface(K, C , bound_Bernstain, cmap=cm.coolwarm,
@@ -159,7 +158,6 @@ ax.set_xlabel('parameter')
 ax.set_ylabel('shrinkage')
 ax.set_zlabel('bound')
 ax.set_title('Bernstein')
-
 
 
 
@@ -180,7 +178,6 @@ ax.scatter(K[-1,j], bound_Chebychev[i,j], s=200, c='k', marker='o')
 ax.set_xlabel('parameter')
 ax.set_ylabel('bound')
 ax.set_title('Chebychev')
-ax.set_ylim([-2000,0])
 
 fig, ax = plt.subplots()
 ax.plot(K[-1,:], bound_Bernstain[-1,:])
@@ -189,5 +186,3 @@ ax.scatter(K[-1,j], bound_Bernstain[i,j], s=200, c='k', marker='o')
 ax.set_xlabel('parameter')
 ax.set_ylabel('bound')
 ax.set_title('Bernstain')
-ax.set_ylim([-2000,0])
-'''

script/run_lqg_onlineoffline_var.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 mdp = LQG1D()
-mdp.horizon = 15
+mdp.horizon = 5
 K_opt = mdp.computeOptimalK()
 
 sb = 1.
@@ -24,12 +24,13 @@ behavioral_policy = GaussianPolicyLinearMeanCholeskyVar(mub, sb)
 target_policy = GaussianPolicyLinearMeanCholeskyVar(mut, st)
 
 
-N = 1000
+N = 200
 
 online_iterations = 10
 offline_iterations = 20
 
 history = []
+lens = [0]
 
 for i in range(online_iterations):
     dataset = collect_episodes(mdp, behavioral_policy, n_episodes=N)
@@ -45,39 +46,79 @@ for i in range(online_iterations):
                                                    delta=0.2,
                                                    behavioral_policy=behavioral_policy,
                                                    importance_weighting_method='is',
-                                                   learning_rate=0.002,
+                                                   learning_rate=0.001,
                                                    estimator='gpomdp',
-                                                   gradient_updater='adam',
+                                                   gradient_updater='vanilla',
                                                    max_iter_opt=offline_iterations,
-                                                   max_iter_eval=500,
+                                                   max_iter_eval=N,
                                                    verbose=2)
 
     initial_parameter = behavioral_policy.get_parameter()
     optimal_parameter, history_offline_reinforce = offline_reinforce_cheb.optimize(
         initial_parameter, return_history=True)
 
+    lens.append(len(history_offline_reinforce)-1)
     history.extend(history_offline_reinforce[:-1])
     behavioral_policy.set_parameter(optimal_parameter)
     target_policy.set_parameter(optimal_parameter)
 
+history.append(history_offline_reinforce[-1])
+_filter = np.cumsum(lens)
+
+target_policy.set_parameter(target_policy.from_param_to_vec(-0.2, 1.))
+online_trajectory_generator = OnlineTrajectoryGenerator(mdp, target_policy)
+
+online_reinforce_cheb = PolicyGradientLearner(online_trajectory_generator,
+                                               target_policy,
+                                               mdp.gamma,
+                                               mdp.horizon,
+                                               select_initial_point=False,
+                                               select_optimal_horizon=False,
+                                               learning_rate=0.001,
+                                               estimator='gpomdp',
+                                               gradient_updater='vanilla',
+                                               max_iter_opt=sum(lens),
+                                               max_iter_eval=N,
+                                               verbose=2)
+
+initial_parameter = target_policy.get_parameter()
+optimal_parameter, history_online_reinforce = online_reinforce_cheb.optimize(
+        initial_parameter, return_history=True)
+
 fig, ax = plt.subplots()
-ax.plot(np.vstack(np.array(history)[:, 0])[:,0], 'r', label='Offline')
-ax.scatter(range(0,offline_iterations*online_iterations,online_iterations), np.vstack(np.array(history)[:, 0])[:,0][range(0,offline_iterations*online_iterations,online_iterations)], marker='o')
+ax.plot(np.vstack(np.array(history)[:, 0])[:, 0], 'r', label='Offline')
+ax.plot(np.vstack(np.array(history_online_reinforce)[:, 0])[:, 0], 'r--', label='Online++')
+ax.plot(_filter, np.vstack(np.array(history_online_reinforce)[:, 0])[:len(_filter), 0], 'r:', label='Online--')
+ax.scatter(_filter, np.vstack(np.array(history)[:, 0])[_filter, 0], c='r', marker='o')
 ax.set_xlabel('Iteration')
 ax.set_ylabel('Parameter')
 legend = ax.legend(loc='upper right')
 
 fig, ax = plt.subplots()
 ax.plot(np.vstack(np.array(history)[:, 0])[:,1], 'b', label='Offline')
-ax.scatter(range(0,offline_iterations*online_iterations,online_iterations), np.vstack(np.array(history)[:, 0])[:,1][range(0,offline_iterations*online_iterations,online_iterations)], marker='o')
+ax.plot(np.vstack(np.array(history_online_reinforce)[:, 0])[:, 1], 'b--', label='Online++')
+ax.plot(_filter, np.vstack(np.array(history_online_reinforce)[:len(_filter), 0])[:, 1], 'b:', label='Online--')
+ax.scatter(_filter, np.vstack(np.array(history)[:, 0])[_filter, 1], c='b', marker='o')
 ax.set_xlabel('Iteration')
 ax.set_ylabel('Std')
 legend = ax.legend(loc='upper right')
 
 fig, ax = plt.subplots()
 ax.plot(np.array(history)[:, 1], 'g', label='Offline')
-ax.scatter(range(0,offline_iterations*online_iterations,online_iterations), np.array(history)[:, 1][range(0,offline_iterations*online_iterations,online_iterations)], marker='o')
+ax.plot(np.array(history_online_reinforce)[:, 1], 'g--', label='Offline++')
+ax.plot(_filter, np.array(history_online_reinforce)[:len(_filter), 1], 'g:', label='Offline--')
+ax.scatter(_filter, np.vstack(np.array(history)[:, 1])[_filter], c='g', marker='o')
 ax.set_xlabel('Iteration')
 ax.set_ylabel('Avg return')
 legend = ax.legend(loc='upper right')
 
+'''
+fig, ax = plt.subplots()
+ax.plot(np.array(history)[:, 1] / np.concatenate([[1], np.repeat(np.arange(len(lens)), lens)]), 'g', label='Offline')
+ax.plot(np.array(history_online_reinforce)[:, 1] / np.arange(1,len(np.array(history_online_reinforce)[:, 1])+1), 'g--', label='Offline++')
+ax.plot(_filter, np.array(history_online_reinforce)[:len(_filter), 1] / np.arange(1,len(_filter)+1), 'g:', label='Offline--')
+ax.set_xlabel('Iteration')
+ax.set_ylabel('Avg return per trajectory')
+legend = ax.legend(loc='upper right')
+
+'''